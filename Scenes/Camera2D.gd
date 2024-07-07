d755db95e88ab0f91f0daea07340ef193facdb29
Kiara <frida.shivaa@gmail.com>
2024-07-07 19:52:06 +0200

    Tile Selection

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -4,10 +4,7 @@ var mouse_start_pos
 var screen_start_position
 
 var dragging = false
-const MaxZoomIn = Vector2(2, 2)
-const MaxZoomOUT = Vector2(0, 0)
-const zoomSteps = Vector2(0.4, 0.4)
-const maxDraggingVal = 1100
+const maxDraggingVal = 2500
 
 func _input(event):
 	if event.is_action("drag"):
@@ -17,22 +14,9 @@ func _input(event):
 			dragging = true
 		else:
 			dragging = false
-			
-	if event.is_action_released("zoom_in"):
-		if zoom.x + zoomSteps.x >= MaxZoomIn.x:
-			zoom = MaxZoomIn
-		else:
-			zoom += zoomSteps
-	elif event.is_action_released("zoom_out"):
-		if zoom.x - zoomSteps.x <= MaxZoomOUT.x:
-			zoom = MaxZoomOUT	
-		else:
-			zoom -= zoomSteps
-	elif event.is_action_released("zoom_reset"):
-		zoom = Vector2(1, 1)
 	elif event is InputEventMouseMotion and dragging:
-		var zoomMultiplayer = 10 if zoom.x < 1 else 1
-		position = zoomMultiplayer * zoom * (mouse_start_pos - event.position) + screen_start_position
+		position =  zoom * (mouse_start_pos - event.position) + screen_start_position
+		
 		if position.x > maxDraggingVal:
 			position.x = maxDraggingVal
 		if position.x < -maxDraggingVal:
--- Scenes/MapScene.gd
+++ Scenes/MapScene.gd
@@ -1,16 +1,22 @@
 extends Node2D
+
+@onready var coordinate_tracker = $Camera2D/CoordinateTracker
+
 @onready var camera_2d = $Camera2D
 @onready var tile_map = $TileMap
 var source_id = 1
 var select_atlas = Vector2i(0,2)
 var water_atlas = Vector2i(0,1)
 var land_atlas = Vector2i(0,0)
+var high_land_atlas = Vector2i(0,3)
 
 @export var noise_height_texture: NoiseTexture2D
 var noise: Noise
 
 var width := 50
 var height := 50
+
+var oldSeelctedTile = Vector2i(99,99)
 
 func _ready():
 	noise = noise_height_texture.noise
@@ -19,11 +25,20 @@ func _ready():
 func _input(event):
 	if(Input.is_action_just_released("left_click")):
 		tile_map.local_to_map(to_local(get_global_mouse_position()))
-		_translate_global_to_tile(get_global_mouse_position())
+		_seelct_tile(get_global_mouse_position())
 				
-func _translate_global_to_tile(global: Vector2):
+func _seelct_tile(global: Vector2):
 	var tilePos = tile_map.local_to_map(to_local(global))
-	tile_map.set_cell(0, tilePos, source_id, select_atlas)
+	if tilePos == oldSeelctedTile:
+		return
+	else:
+		var beforeAtlas = tile_map.get_cell_atlas_coords(0, oldSeelctedTile) - Vector2i(1, 0)
+		tile_map.set_cell(0, oldSeelctedTile, source_id, beforeAtlas)
+		oldSeelctedTile = tilePos
+
+	var afterAtlas = tile_map.get_cell_atlas_coords(0, tilePos) + Vector2i(1, 0)
+	tile_map.set_cell(0, tilePos, source_id, afterAtlas)
+	coordinate_tracker.text = str(tilePos)
 
 func _generate_world():
 	for x in range(-width/2 , width/2):
@@ -31,5 +46,7 @@ func _generate_world():
 			var noiseValue := noise.get_noise_2d(2*x, 2*y)
 			if noiseValue >= 0.0: # land
 				tile_map.set_cell(0, Vector2(x,y), source_id, land_atlas)
+			if noiseValue >= 0.2: # high land
+				tile_map.set_cell(0, Vector2(x,y), source_id, high_land_atlas)
 			elif noiseValue < 0.0: # water
 				tile_map.set_cell(0, Vector2(x,y), source_id, water_atlas)
