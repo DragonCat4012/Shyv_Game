88264cc24dd5e15efea91edeba09398080e8aa73
Kiara <frida.shivaa@gmail.com>
2024-07-07 19:21:40 +0200

    fix Zoom

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -5,8 +5,9 @@ var screen_start_position
 
 var dragging = false
 const MaxZoomIn = Vector2(2, 2)
-const MaxZoomOUT = Vector2(-2, -2)
-const zoomSteps = Vector2(0.7, 0.7)
+const MaxZoomOUT = Vector2(0, 0)
+const zoomSteps = Vector2(0.4, 0.4)
+const maxDraggingVal = 1100
 
 func _input(event):
 	if event.is_action("drag"):
@@ -30,6 +31,16 @@ func _input(event):
 	elif event.is_action_released("zoom_reset"):
 		zoom = Vector2(1, 1)
 	elif event is InputEventMouseMotion and dragging:
-		position = zoom * (mouse_start_pos - event.position) + screen_start_position
+		var zoomMultiplayer = 10 if zoom.x < 1 else 1
+		position = zoomMultiplayer * zoom * (mouse_start_pos - event.position) + screen_start_position
+		if position.x > maxDraggingVal:
+			position.x = maxDraggingVal
+		if position.x < -maxDraggingVal:
+			position.x = -maxDraggingVal
+			
+		if position.y > maxDraggingVal:
+			position.y = maxDraggingVal
+		if position.y < -maxDraggingVal:
+			position.y = -maxDraggingVal
 		
-	print(zoom)
+	#print(zoom)
--- Scenes/MapScene.gd
+++ Scenes/MapScene.gd
@@ -1,19 +1,32 @@
 extends Node2D
-
+@onready var camera_2d = $Camera2D
 @onready var tile_map = $TileMap
 var source_id = 1
+var select_atlas = Vector2i(0,2)
 var water_atlas = Vector2i(0,1)
 var land_atlas = Vector2i(0,0)
 
 @export var noise_height_texture: NoiseTexture2D
 var noise: Noise
 
-var width := 100
-var height := 100
+var width := 50
+var height := 50
 
 func _ready():
 	noise = noise_height_texture.noise
 	_generate_world()
+
+func _input(event):
+	if(Input.is_action_just_released("left_click")):
+		_translate_global_to_tile(get_global_mouse_position())
+		#var tile = local_to_map(get_global_mouse_position());
+				#something here to get the clicked physics layer?
+				
+func _translate_global_to_tile(global: Vector2):
+	var tilePos = round(global / 125 / 2)
+	print(global, " -> ",tilePos)
+	tile_map.set_cell(0, tilePos, source_id, select_atlas)
+	pass
 
 func _generate_world():
 	for x in range(-width/2 , width/2):
