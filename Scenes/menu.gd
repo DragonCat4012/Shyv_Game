a843d253413f1c418a7121587ef32d094938f37f
Kiara <frida.shivaa@gmail.com>
2024-07-08 12:14:05 +0200

    Move Files

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 func _on_join_button_pressed():
 	GamManager._on_join_pressed()
-	get_tree().change_scene_to_file("res://Scenes/PreGame/PeerView.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Lobby/PeerView.tscn")
 
 func _on_host_button_pressed():
 	GamManager._on_host_pressed()
-	get_tree().change_scene_to_file("res://Scenes/PreGame/hostView.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Lobby/hostView.tscn")
 
 func _on_test_pressed():
-	get_tree().change_scene_to_file("res://Scenes/map.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Game/map.tscn")
