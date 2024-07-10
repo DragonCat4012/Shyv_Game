79ef0b7f4aec32a3ba7bd7cb358189d783c8c99c
Kiara <frida.shivaa@gmail.com>
2024-07-10 18:14:40 +0200

    Add nation mdoel parsing

--- Global/JSONUTIL.gd
+++ Global/JSONUTIL.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var BuildingTiles = preload("res://Models/BuildingTiles.gd")
+const BuildingTiles = preload("res://Models/BuildingTiles.gd")
 
 func BuildingTile_to_JSON(tile: BuildingTiles) -> String: # TODO: stringify Building
 	var dict = {"ownedByNationID": tile.ownedByNationID, "coords": { "x": tile.coords.x, "y": tile.coords.y}, "building": ""}
@@ -9,6 +9,28 @@ func BuildingTile_to_JSON(tile: BuildingTiles) -> String: # TODO: stringify Buil
 func BuildingTiles_from_JSON(str: String) -> BuildingTiles:
 	var tile = BuildingTiles.new()
 	var dict = JSON.parse_string(str)
+	if not dict:
+		print(">>> Failed to parse Tile <<<")
+		return null
 	tile.ownedByNationID = dict["ownedByNationID"]
 	tile.coords = Vector2(dict["coords"]["x"], dict["coords"]["y"])
 	return tile
+	
+	
+	
+const NationModel = preload("res://Models/NationModel.gd")
+func NationModel_to_JSON(nation: NationModel) -> String:
+	var dict = {"name": nation.name, "description": nation.description, "color": nation.color, "building_tile_row": nation.building_tile_row}
+	return JSON.stringify(dict)
+	
+func NationModel_from_JSON(str: String) -> NationModel:
+	var nation = NationModel.new()
+	var dict = JSON.parse_string(str)
+	if not dict:
+		print(">>> Failed to parse Nation <<<")
+		return null
+	nation.name = dict["name"]
+	nation.description = dict["description"]
+	nation.color = dict["color"]
+	nation.building_tile_row = dict["building_tile_row"]
+	return nation
--- Models/NationModel.gd
+++ Models/NationModel.gd
@@ -3,5 +3,4 @@ class_name NationModel extends Resource
 @export var name: String = "Nameless Nation"
 @export var description: String = "Nameless Description"
 @export var color: Color = Color.REBECCA_PURPLE
-
-@export var tile_set: TileSet
+@export var building_tile_row: int = 0
