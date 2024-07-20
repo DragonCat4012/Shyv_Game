5b4ae4fd447548bf0a17bb1f180c322003d29f9e
Kiara <frida.shivaa@gmail.com>
2024-07-20 19:15:46 +0200

    update menu UI +  add version label

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -1,5 +1,14 @@
 extends Node2D
+@onready var version_label = $VersionLabel
 
+func _ready():
+	# Load version Info
+	var versionInfo = load("res://version.txt")
+	var file = FileAccess.open("res://version.txt", FileAccess.READ)
+	var version = file.get_as_text()
+	file.close()
+	version_label.text = version
+	
 func _on_join_button_pressed():
 	GamManager._on_join_pressed()
 	get_tree().change_scene_to_file(SceneManager.PEERSCENE)
