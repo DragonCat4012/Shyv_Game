853662fd434fae4429b1e8743ef7fc61c99a0ba7
Kiara <frida.shivaa@gmail.com>
2024-07-21 11:34:00 +0200

    add event names

--- Scenes/Overlays/GMOptions/EventOptions.gd
+++ Scenes/Overlays/GMOptions/EventOptions.gd
@@ -1,16 +1,23 @@
 extends Panel
+@onready var btn_event_1 = $VBoxContainer/BtnEvent1
+@onready var btn_event_2 = $VBoxContainer/BtnEvent2
+@onready var btn_event_3 = $VBoxContainer/BtnEvent3
 
+var options = ["aloha1","aloha2","aloha3"]
 
 func _on_btn_event_1_pressed():
-	EventSystem.EVENT_SELECTED.emit("aloha1")
+	EventSystem.EVENT_SELECTED.emit(options[0])
 
 func _on_btn_event_2_pressed():
-	EventSystem.EVENT_SELECTED.emit("aloha2")
+	EventSystem.EVENT_SELECTED.emit(options[1])
 
 func _on_btn_event_3_pressed():
-	EventSystem.EVENT_SELECTED.emit("aloha3")
-
+	EventSystem.EVENT_SELECTED.emit(options[1])
 
 func _on_visibility_changed():
-	# TODO: generate new Events?
-	print("update event visibillity")
+	if visible:
+		var events = EventManager.get_random_events()
+		options = events
+		btn_event_1.text = events[0]
+		btn_event_2.text = events[1]
+		btn_event_3.text = events[2]
--- Scenes/Overlays/PlayerOverlays/EventDialogueScene.gd
+++ Scenes/Overlays/PlayerOverlays/EventDialogueScene.gd
@@ -1,4 +1,15 @@
 extends Control
 
+@onready var description = $VBoxContainer/Description
+@onready var title = $VBoxContainer/Title
+
+func _ready():
+	EventSystem.EVENT_OCCURED.connect(_on_event_occured)
+
 func _on_accept_button_pressed():
 	EventSystem.EVENT_ACCEPTED.emit()
+
+func _on_event_occured(eventName):
+	print("hm ", eventName)
+	title.text = eventName
+	description.text = EventManager.get_event_description_from_name(eventName)
