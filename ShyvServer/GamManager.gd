ca1bfb3782e5a1ab2a47ae4e1da56216fff0d5d7
Kiara <sophie_natalie.zieschang@mailbox.tu-dresden.de>
2024-07-24 18:22:29 +0200

    fix server

--- ShyvServer/GamManager.gd
+++ ShyvServer/GamManager.gd
@@ -15,10 +15,10 @@ func _ready():
 func start_server():
 	network.create_server(port, 100)
 	multiplayer.multiplayer_peer = network
-	
+	get_tree().set_multiplayer(multiplayer)
 	print("Server started")
 	#print(IP.get_local_addresses())
-	#print(IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1))
+	print(IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1))
 
 func _peer_connected(player_id):
 	print("> ", player_id, " Connected")
@@ -35,19 +35,43 @@ Authority Client → Server: @rpc("authority", "call_remote", "reliable")
 '''
 
 # Lobbies
-@rpc#("any_peer", "call_remote", "reliable")
-func _on_lobby_create():
+@rpc("any_peer", "reliable")
+func on_lobby_create():
 	var peer_id = multiplayer.get_remote_sender_id()
 	print("New lobby created: ", peer_id)
 	lobbies[peer_id] = []
 	
-@rpc("any_peer", "call_remote", "reliable")
-func _on_lobby_joined(lobbyID):
+@rpc("any_peer",  "reliable")
+func on_lobby_joined(lobbyID):
 	var peer_id = multiplayer.get_remote_sender_id()
 	print(peer_id," joined lobby ", lobbyID)
+	
 	var oldPlayers = lobbies[lobbyID]
 	oldPlayers.append(peer_id)
 	lobbies[lobbyID] = oldPlayers
 
 func _send_joined_players():
 	pass # TODO: publsih othger joined players
+
+# MARK idk
+
+#@rpc
+func _on_peer_disconnect(peerID):
+	pass
+
+#@rpc
+func _on_player_connected(new_per_id):
+	pass
+	
+#@rpc
+func _on_player_diconnected(new_per_id):
+	pass
+
+#@rpc # only server can call it
+func add_previously_connected_player_character(peer_ids):
+	pass
+	
+#@rpc
+func add_previously_send_messages(msgs):
+	pass
+	
