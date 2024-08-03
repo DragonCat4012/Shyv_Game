7fe5e629906f6a86dbb044d698ed5dc07d634520
Kiara <frida.shivaa@gmail.com>
2024-08-03 20:49:47 +0200

    fix tile erros

--- Global/RessourceManager.gd
+++ Global/RessourceManager.gd
@@ -6,7 +6,3 @@ var THEME_BUTTON_DELETE = preload("res://Graphics/Themes/deleteTheme.tres")
 var BUILDING_TILES = preload("res://Models/BuildingTiles.gd")
 var START_POSITIONS_GENERATOR = preload("res://Scenes/Game/StartPositionsGenerate.gd")
 var LOADED_NATION_MODEL = preload("res://Models/NationModel.gd")
-
-# Grafiks
-const GAMEPHASES_ACTIVE = preload("res://Graphics/gamephases_active.png")
-const GAMEPHASES = preload("res://Graphics/gamephases.png")
